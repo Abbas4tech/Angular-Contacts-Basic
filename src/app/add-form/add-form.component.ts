fadd250120c40dee645adcd4de7fcf4a349d8f44
Abbas4tech <shaikhabbas54@gmail.com>
2022-04-02 01:27:45 +0530

    Basic Add form created

---
+++ src/app/add-form/add-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-form',
+  templateUrl: './add-form.component.html',
+  styleUrls: ['./add-form.component.css'],
+})
+export class AddFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ButtonComponent } from './UI/button/button.component';
+import { AddFormComponent } from './add-form/add-form.component';
+import { CardComponent } from './UI/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    AddFormComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
